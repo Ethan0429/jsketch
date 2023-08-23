ab5461bfb31e7f6769c3661140c10ce6a75a4704
Ethan Rickert <ethanrickert0429@gmail.com>
2023-08-23 19:22:44 -0400

    added grid config instead of constants for grid params

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const GRID_WIDTH = 16;
-const GRID_HEIGHT = 16;
+const GridConfig = {
+  gridWidth: 16,
+  gridHeight: 16,
+};
 
 const grid = document.querySelector("#grid");
 
 function createGrid() {
-  for (let i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++) {
+  for (let i = 0; i < GridConfig.gridWidth * GridConfig.gridHeight; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     grid.appendChild(cell);
