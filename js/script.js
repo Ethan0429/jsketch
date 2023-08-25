4670b618fb5db1aa57d41be6e42114238ca7660f
Ethan Rickert <ethanrickert0429@gmail.com>
2023-08-24 21:37:55 -0400

    misc - marked tiles are no longer selectable

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const GridConfig = {
-  gridWidth: 16,
-  gridHeight: 16,
+	gridWidth: 16,
+	gridHeight: 16,
 };
 
 const canvasSize = 900;
@@ -9,65 +9,67 @@ const gridSettingsButton = document.querySelector("#grid-settings");
 const modal = document.querySelector(".modal");
 
 function addEventListeners() {
-  gridSettingsButton.addEventListener("click", (e) => {
-    modal.style.display = "flex";
+	gridSettingsButton.addEventListener("click", (e) => {
+		modal.style.display = "flex";
 
-    // Close modal
-    const modalClose = document.querySelector(".close-icon");
-    modalClose.addEventListener("click", () => {
-      modal.style.display = "none";
-    });
+		// Close modal
+		const modalClose = document.querySelector(".close-icon");
+		modalClose.addEventListener("click", () => {
+			modal.style.display = "none";
+		});
 
-    // Change grid size
-    const gridWidthInput = document.querySelector("#width");
-    const gridHeightInput = document.querySelector("#height");
-    const gridChangeButton = document.querySelector("#confirm");
-    gridChangeButton.addEventListener("click", () => {
-      GridConfig.gridWidth = gridWidthInput.value;
-      GridConfig.gridHeight = gridHeightInput.value;
-      destroyGrid();
-      createGrid();
-      modal.style.display = "none";
-    });
-  });
+		// Change grid size
+		const gridWidthInput = document.querySelector("#width");
+		const gridHeightInput = document.querySelector("#height");
+		const gridChangeButton = document.querySelector("#confirm");
+		gridChangeButton.addEventListener("click", () => {
+			GridConfig.gridWidth = gridWidthInput.value;
+			GridConfig.gridHeight = gridHeightInput.value;
+			destroyGrid();
+			createGrid();
+			modal.style.display = "none";
+		});
+	});
 }
 
 function createGrid() {
-  for (let i = 0; i < GridConfig.gridWidth * GridConfig.gridHeight; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    grid.appendChild(cell);
-  }
+	for (let i = 0; i < GridConfig.gridWidth * GridConfig.gridHeight; i++) {
+		const cell = document.createElement("div");
+		cell.classList.add("cell");
+		grid.appendChild(cell);
+	}
+	grid.style.width = `${canvasSize}px`;
+	grid.style.height = `${canvasSize}px`;
 
-  let mouseIsDown = false;
-  const cells = document.querySelectorAll(".cell");
+	let mouseIsDown = false;
+	const cells = document.querySelectorAll(".cell");
 
-  // dynamically size cells
-  cells.forEach((cell) => {
-    cell.style.width = `${canvasSize / GridConfig.gridWidth}px`;
-    cell.style.height = `${canvasSize / GridConfig.gridHeight}px`;
-  });
+	// dynamically size cells
+	cells.forEach((cell) => {
+		cell.style.width = `${canvasSize / GridConfig.gridWidth}px`;
+		cell.style.height = `${canvasSize / GridConfig.gridHeight}px`;
+	});
 
-  cells.forEach((cell) => {
-    cell.addEventListener("mousedown", function () {
-      mouseIsDown = true;
-      cell.style.backgroundColor = "black";
-    });
+	cells.forEach((cell) => {
+		cell.addEventListener("mousedown", function () {
+			mouseIsDown = true;
+			cell.classList.add("marked");
+		});
 
-    cell.addEventListener("mouseup", function () {
-      mouseIsDown = false;
-    });
+		cell.addEventListener("mouseup", function () {
+			mouseIsDown = false;
+		});
 
-    cell.addEventListener("mousemove", function (event) {
-      if (mouseIsDown) {
-        cell.style.backgroundColor = "black";
-      }
-    });
-  });
+		cell.addEventListener("mousemove", function (event) {
+			if (mouseIsDown) {
+				cell.classList.add("marked");
+			}
+		});
+	});
 }
 
 function destroyGrid() {
-  grid.innerHTML = "";
+	grid.innerHTML = "";
 }
 
 createGrid();
